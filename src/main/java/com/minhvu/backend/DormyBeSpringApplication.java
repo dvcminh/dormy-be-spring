eaeedc0c72853e7b81617dc54874326225308839
Vũ Đức Minh <vuducminh210503@gmail.com>
2024-06-22 23:40:58 +0700

    init project

---
+++ src/main/java/com/minhvu/backend/DormyBeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.minhvu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormyBeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormyBeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minhvu/backend/DormyBeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhvu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormyBeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
