55a8a5b2edf36c84d391287a5086ea62fba45a65
Vũ Đức Minh <vuducminh210503@gmail.com>
2024-10-04 23:44:06 +0700

    feat: update post in feed service

--- feed/src/main/java/com/minhvu/feed/model/PostEntity.java
+++ feed/src/main/java/com/minhvu/feed/model/PostEntity.java
@@ -1,11 +1,13 @@
 package com.minhvu.feed.model;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
@@ -18,5 +20,7 @@ public class PostEntity extends BaseEntity {
     private String body;
 
     private UUID userId;
+    @ElementCollection
+    private Collection<String> urlsMedia;
     private boolean isDeleted;
 }
